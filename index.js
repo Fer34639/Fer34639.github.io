add45de0b1b3416a2c57df9000c367277d6d63df
Fer34639 <126346063+Fer34639@users.noreply.github.com>
2023-02-26 14:31:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+		document.getElementById('inicio').onclick = function () {
+		document.getElementById('inicio2').style.display = "block";
+				document.getElementById('datos2').style.display = "none"; 
+		document.getElementById('formacion2').style.display = "none"; 
+		document.getElementById('experiencia2').style.display = "none"; 
+
+	}
+
+		document.getElementById('datos').onclick = function () {
+		document.getElementById('inicio2').style.display = "none"; 
+		document.getElementById('datos2').style.display = "block"; 
+		document.getElementById('formacion2').style.display = "none"; 
+		document.getElementById('experiencia2').style.display = "none"; 
+
+	}
+		document.getElementById('formacion').onclick = function () {
+		document.getElementById('inicio2').style.display = "none"; 
+		document.getElementById('datos2').style.display = "none"; 
+		document.getElementById('formacion2').style.display = "block"; 
+		document.getElementById('experiencia2').style.display = "none"; 
+
+	}
+		document.getElementById('experiencia').onclick = function () {
+		document.getElementById('inicio2').style.display = "none"; 
+		document.getElementById('datos2').style.display = "none"; 
+		document.getElementById('formacion2').style.display = "none"; 
+		document.getElementById('experiencia2').style.display = "block"; 
+
+	}
+
